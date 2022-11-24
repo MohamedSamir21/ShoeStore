a44d7b1c1180094a497eac72485ea9c7284a464f
mo <mo@gmail.com>
2022-11-24 03:04:36 +0200

    Initial commit

--- app/src/main/java/com/example/shoestore/MainActivity.kt
+++ app/src/main/java/com/example/shoestore/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.example.shoestore.databinding.ActivityMainBinding
+import com.example.shoestore.databinding.FragmentShoeListBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration : AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        // prevent nav gesture if not on start destination
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, bundle: Bundle? ->
+            if (nd.id == nc.graph.getStartDestination()) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 }
--- app/src/main/java/com/example/shoestore/ShoeList.kt
+++ app/src/main/java/com/example/shoestore/ShoeList.kt
@@ -1,59 +1,75 @@
 package com.example.shoestore
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ShoeList.newInstance] factory method to
- * create an instance of this fragment.
- */
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.example.shoestore.databinding.FragmentShoeListBinding
+import android.support.v4.*
 class ShoeList : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentShoeListBinding
+    private val shoeViewModel: ShoeViewModel by activityViewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                savedInstanceState: Bundle?): View? {
+        // Inflate layout and obtain an instance of the binding class.
+        binding = DataBindingUtil.inflate(
+        inflater, R.layout.fragment_shoe_list, container, false)
+
+        binding.setLifecycleOwner(this)
+
+        setHasOptionsMenu(true)
+
+        binding.addFAB.setOnClickListener{
+            findNavController().navigate(R.id.action_shoeList_to_shoeDetail)
         }
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shoe_list, container, false)
-    }
+        shoeViewModel.shoes.observe(viewLifecycleOwner, Observer {
+            if (it != null){
+                for (index in 0 .. (shoeViewModel.shoeListw!!.size)-1){
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ShoeList.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ShoeList().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    val linearLayout = binding.linearLayout
+                    val customView = layoutInflater.inflate(R.layout.custom_item, null, false)
+
+
+                    val shoeNameTextView = customView.findViewById<TextView>(R.id.shoeNameTextView)
+                    shoeNameTextView.text = "Name: " + shoeViewModel.shoeListw?.elementAt(index)?.name.toString()
+
+                    val companyTextView = customView.findViewById<TextView>(R.id.companyTextView)
+                    companyTextView.text = "Company: " + shoeViewModel.shoeListw?.elementAt(index)?.company.toString()
+
+                    val shoeSizeTextView = customView.findViewById<TextView>(R.id.shoeSizeTextView)
+                    shoeSizeTextView.text = "Size: " + shoeViewModel.shoeListw?.elementAt(index)?.size.toString()
+
+                    val shoeDescriptionTextView = customView.findViewById<TextView>(R.id.shoeDescriptionTextView)
+                    shoeDescriptionTextView.text = "Description: " + shoeViewModel.shoeListw?.elementAt(index)?.description.toString()
+
+                    customView.setLayoutParams(LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT));
+
+                    linearLayout.addView(customView);
+
                 }
             }
+
+        })
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.navdrawer_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.logut -> findNavController().navigate(R.id.action_shoeList_to_login)
+        }
+        return true
     }
 }
